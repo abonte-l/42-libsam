8ea378d161eacd6fb7ac2744048a113c5ff692ec
abonte-l <abonte-l@student.42.fr>
2021-04-27 14:59:25 +0200

    add ft_strlen

---
+++ src/ft_strlen.s
@@ -0,0 +1,12 @@
+section .text
+		global ft_strlen
+start:
+	mov rcx, 0 ;initialisation de rcx a 0
+comp:
+	cmp BYTE [rdi + rcx], 0 ; compare le char (BYTE) de str[i] ([rdi + rcx])
+	jz end ; si la comparaison est egale a 0 alors on va directement a end:
+	inc rcx ;incrementation de rcx
+	jmp comp ; on retourne au debut de comp:
+end:
+	mov rax, rcx ; on donne la valeur de retour rcx(registre compteur) a rax (registre retour de fonction)
+	ret
