689b49c8a8372570e3f001ce12e99f39941825f6
abonte-l <abonte-l@student.42.fr>
2021-04-27 15:11:36 +0200

    add ft_strlen

--- src/ft_strlen.s
+++ src/ft_strlen.s
@@ -3,7 +3,7 @@ section .text
 start:
 	mov rcx, 0 ;initialisation de rcx a 0
 comp:
-	cmp BYTE [rdi + rcx], 0 ; compare le char (BYTE) de str[i] ([rdi + rcx])
+	cmp BYTE [rdi + rcx], 0 ; compare le char (BYTE) de str[i] ([rdi + rcx]) rdi est le registre par defaut pour l'argument 1 de l fonction
 	jz end ; si la comparaison est egale a 0 alors on va directement a end:
 	inc rcx ;incrementation de rcx
 	jmp comp ; on retourne au debut de comp:
